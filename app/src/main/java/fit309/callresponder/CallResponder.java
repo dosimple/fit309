678c05730c454a905c0322676e0d2a77d656085e
Olzvoi Bayasgalan <olzvoi@gmail.com>
2018-05-07 18:01:17 +0800

    Implement and use "endCall"

--- app/src/main/java/fit309/callresponder/CallResponder.java
+++ app/src/main/java/fit309/callresponder/CallResponder.java
@@ -5,11 +5,15 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.telephony.TelephonyManager;
+import android.util.Log;
 import android.widget.Toast;
+
+import java.lang.reflect.Method;
 
 
 public class CallResponder extends BroadcastReceiver {
 
+    private static final String TAG = "CallResponder";
     SharedPreferences pref;
 
     @Override
@@ -26,6 +30,22 @@ public class CallResponder extends BroadcastReceiver {
             return;
 
         String number = intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER);
-        Toast.makeText(context, "Incomming: " + number + ", Message: " + message, Toast.LENGTH_LONG).show();
+
+        try {
+            endCall(context);
+            Toast.makeText(context, "Incomming call: " + number, Toast.LENGTH_LONG).show();
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(context, "Error: " + e.getMessage(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    protected void endCall(Context context) throws Exception {
+        TelephonyManager telephonyManager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+        Method getITelephony = TelephonyManager.class.getDeclaredMethod("getITelephony");
+        getITelephony.setAccessible(true);
+        Object telephony = getITelephony.invoke(telephonyManager);
+        Method endCall = telephony.getClass().getDeclaredMethod("endCall");
+        endCall.invoke(telephony);
     }
 }
